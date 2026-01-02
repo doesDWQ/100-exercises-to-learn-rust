92dfdb2b3860abe9335872142446a84b708cfb98
dwq123 <2354513578@qq.com>
2026-01-02 22:04:13 +0800

    auto-commit

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,11 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut ret = 1;
+    for item in 1..=n {
+        ret *= item
+    }
+    
+    return ret
 }
 
 #[cfg(test)]
