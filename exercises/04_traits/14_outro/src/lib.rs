7cb324f06427b8707856b1ab22cb70a39887f571
dwq <2354513578@qq.com>
2026-01-03 22:31:41 +0800

    auto-commit

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,6 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+struct SaturatingU16{}
+
