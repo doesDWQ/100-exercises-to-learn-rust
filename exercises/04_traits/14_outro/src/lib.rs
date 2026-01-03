29fc4b9c60ea7316d2e91c9367f638cb235ede45
dwq <2354513578@qq.com>
2026-01-03 23:04:13 +0800

    auto-commit

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -42,7 +42,7 @@ impl Add for SaturatingU16 {
     type Output = SaturatingU16;
 
     fn add(self, rhs: Self) -> Self::Output {
-        SaturatingU16(self.0.wrapping_add(rhs.0))
+        SaturatingU16(self.0.saturating_add(rhs.0))
     }
 }
 
@@ -58,7 +58,7 @@ impl Add<&SaturatingU16> for SaturatingU16 {
     type Output = SaturatingU16;
 
     fn add(self, rhs: &SaturatingU16) -> Self::Output {
-        SaturatingU16(self.0.saturating_add(rhs.0))
+        SaturatingU16(self.0.wrapping_add(rhs.0))
     }
 }
 
