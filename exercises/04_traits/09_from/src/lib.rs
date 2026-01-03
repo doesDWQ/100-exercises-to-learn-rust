b2121c3de9d0513296c69556f716c54fbbdd13d3
dwq <2354513578@qq.com>
2026-01-03 20:43:08 +0800

    auto-commit

--- exercises/04_traits/09_from/src/lib.rs
+++ exercises/04_traits/09_from/src/lib.rs
@@ -4,7 +4,11 @@ pub struct WrappingU32 {
     value: u32,
 }
 
-impl
+impl From<u32> for WrappingU32 {
+    fn from(value: u32) -> Self {
+        return WrappingU32 { value }
+    }
+}
 
 fn example() {
     let wrapping: WrappingU32 = 42.into();
