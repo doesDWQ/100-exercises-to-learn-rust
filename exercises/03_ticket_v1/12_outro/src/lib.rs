0597bf1cef190dbee160af3ccfdff6d5b8366d5e
dwq123 <2354513578@qq.com>
2026-01-03 11:53:00 +0800

    auto-commit

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -12,126 +12,34 @@
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
 
-
-pub struct Ticket {
-    title: String,
-    description: String,
-    status: String,
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
 }
 
-impl Ticket {
-    pub fn new(title: String, description: String, status: String) -> Ticket {
-        Self::verify_title(&title);
-        Self::verify_description(&description);
-        Self::verify_status(&status);
-
-        Ticket {
-            title,
-            description,
-            status,
-        }
-    }
-
-    pub fn title(&self) -> &String {
-        &self.title
-    }
-
-    pub fn description(&self) -> &String {
-        &self.description
-    }
-
-    pub fn status(&self) -> &String {
-        &self.status
-    }
-
-    pub fn set_title(&mut self, title: String) {
-        Self::verify_title(&title);
+impl Order {
+    pub fn new(product_name: String,
+    quantity: u32,
+    unit_price: u32) ->Order {
         
-        self.title = title;
-    }
 
-    fn verify_title(title: &String){
-        if title.len() == 0 {
-            panic!("Title cannot be empty")
-        }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes")
+        Order {
+            product_name,
+            quantity,
+            unit_price,
         }
     }
 
-    pub fn set_description(&mut self, description: String) {
-        Self::verify_description(&description);
-        self.description = description;
+    fn verify_product_name(&self, product_name: &String){
+        
     }
 
-    fn verify_description(description: &String){
-        if description.len() == 0 {
-            panic!("Description cannot be empty")
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes")
-        }
+    fn verify_quantity(&self, quantity: &u32){
+        
     }
 
-    pub fn set_status(&mut self, status: String) {
-        Self::verify_status(&status);
-
-        self.status = status;
-    }
-
-    fn verify_status(status: &String){
-        if status != "Done" && status != "In Progress" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed")
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::Ticket;
-    use common::{overly_long_description, overly_long_title, valid_description, valid_title};
-
-    #[test]
-    fn works() {
-        let mut ticket = Ticket::new("A title".into(), "A description".into(), "To-Do".into());
-        ticket.set_title("A new title".into());
-        ticket.set_description("A new description".into());
-        ticket.set_status("Done".into());
-
-        assert_eq!(ticket.title(), "A new title");
-        assert_eq!(ticket.description(), "A new description");
-        assert_eq!(ticket.status(), "Done");
-    }
-
-    #[test]
-    #[should_panic(expected = "Title cannot be empty")]
-    fn title_cannot_be_empty() {
-        Ticket::new(valid_title(), valid_description(), "To-Do".into()).set_title("".into());
-    }
-
-    #[test]
-    #[should_panic(expected = "Description cannot be empty")]
-    fn description_cannot_be_empty() {
-        Ticket::new(valid_title(), valid_description(), "To-Do".into()).set_description("".into());
-    }
-
-    #[test]
-    #[should_panic(expected = "Title cannot be longer than 50 bytes")]
-    fn title_cannot_be_longer_than_fifty_chars() {
-        Ticket::new(valid_title(), valid_description(), "To-Do".into())
-            .set_title(overly_long_title())
-    }
-
-    #[test]
-    #[should_panic(expected = "Description cannot be longer than 500 bytes")]
-    fn description_cannot_be_longer_than_500_chars() {
-        Ticket::new(valid_title(), valid_description(), "To-Do".into())
-            .set_description(overly_long_description())
-    }
-
-    #[test]
-    #[should_panic(expected = "Only `To-Do`, `In Progress`, and `Done` statuses are allowed")]
-    fn status_must_be_valid() {
-        Ticket::new(valid_title(), valid_description(), "To-Do".into()).set_status("Funny".into());
+    fn verify_unit_price(&self, unit_price: &u32){
+        
     }
 }
