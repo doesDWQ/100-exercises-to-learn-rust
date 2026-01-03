99c0a50e73ae061c26e77e310d921954f2c380c6
dwq123 <2354513578@qq.com>
2026-01-03 13:22:21 +0800

    auto-commit

--- exercises/04_traits/05_trait_bounds/src/lib.rs
+++ exercises/04_traits/05_trait_bounds/src/lib.rs
@@ -6,10 +6,21 @@
 // collections (e.g. BTreeMap).
 
 /// Return the minimum of two values.
-pub fn min<T>(left: T, right: T) -> T {
+pub fn min<T>(left: T, right: T) -> T 
+where T: std::cmp::PartialOrd {
     if left <= right {
         left
     } else {
         right
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_min() {
+        assert_eq!(min(1, 2), 1);
     }
 }
