538e7bec158feeab76698a472fef2f1649840a1b
dwq <2354513578@qq.com>
2026-01-04 22:08:41 +0800

    auto-commit

--- exercises/05_ticket_v2/01_enum/src/lib.rs
+++ exercises/05_ticket_v2/01_enum/src/lib.rs
@@ -1,21 +1,26 @@
 // TODO: use `Status` as type for `Ticket::status`
 //   Adjust the signature and implementation of all other methods as necessary.
 
+use std::os::macos::raw::stat;
+
 #[derive(Debug, PartialEq)]
 // `derive`s are recursive: it can only derive `PartialEq` if all fields also implement `PartialEq`.
 // Same holds for `Debug`. Do what you must with `Status` to make this work.
 struct Ticket {
     title: String,
     description: String,
-    status: String,
+    status: Status,
 }
 
+#[derive(Debug, PartialEq)]
 enum Status {
-    // TODO: add the missing variants
+    ToDo,
+    InProgress,
+    Done,
 }
 
 impl Ticket {
-    pub fn new(title: String, description: String, status: String) -> Ticket {
+    pub fn new(title: String, description: String, status: Status) -> Ticket {
         if title.is_empty() {
             panic!("Title cannot be empty");
         }
@@ -28,9 +33,15 @@ impl Ticket {
         if description.len() > 500 {
             panic!("Description cannot be longer than 500 bytes");
         }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
+
+        // if match status {
+        //     Status::Done => | s | => { println!("{:?}", s) },
+        //     Status::ToDo => | s | => { println!("{:?}", s) },
+        //     Status::InProgress => |status| => { println!("{:?}", s) },
+        // }
+        // if status != Status::ToDo && status != Status::InProgress && status != Status::Done {
+        //     panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        // }
 
         Ticket {
             title,
@@ -47,7 +58,7 @@ impl Ticket {
         &self.description
     }
 
-    pub fn status(&self) -> &String {
+    pub fn status(&self) -> &Status {
         &self.status
     }
 }
@@ -83,6 +94,7 @@ mod tests {
             description: "description".to_string(),
             status,
         };
+        let status = Status::ToDo;
         let ticket2 = Ticket {
             title: title.clone(),
             description: "description2".to_string(),
@@ -100,6 +112,7 @@ mod tests {
             description: description.clone(),
             status,
         };
+        let status = Status::InProgress;
         let ticket2 = Ticket {
             title: "title2".to_string(),
             description: description.clone(),
