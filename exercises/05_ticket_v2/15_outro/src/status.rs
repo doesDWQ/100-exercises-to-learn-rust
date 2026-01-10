ee162dfd659827268ae0e043e47a9750e629c643
dwq123 <2354513578@qq.com>
2026-01-10 21:17:45 +0800

    auto-commit

--- exercises/05_ticket_v2/15_outro/src/status.rs
+++ exercises/05_ticket_v2/15_outro/src/status.rs
@@ -16,7 +16,7 @@ pub enum Status {
 
 impl Display for Status {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        write!(f, "hello world!")
     }
 }
 
