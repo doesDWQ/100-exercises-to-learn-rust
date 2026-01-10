f2dead6b834db54533311f5e7f75df9c253d2a1f
dwq123 <2354513578@qq.com>
2026-01-10 12:44:03 +0800

    auto-commit

--- exercises/05_ticket_v2/11_dependencies/src/lib.rs
+++ exercises/05_ticket_v2/11_dependencies/src/lib.rs
@@ -4,3 +4,13 @@
 // When you import a type (`Error`) from a dependency, the import path must start
 // with the crate name (`anyhow`, in this case).
 use anyhow::Error;
+
+#[cfg(test)]
+mod tests {
+    use anyhow::Error;
+    
+    #[test]
+    fn dwq(){
+        println!("{:?}", Error::Known)
+    }
+}
