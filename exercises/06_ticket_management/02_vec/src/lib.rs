67b362e11fa02a4389573cab895da3d12323273b
dwq123 <2354513578@qq.com>
2026-01-10 22:26:53 +0800

    auto-commit

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -10,12 +10,18 @@
 //
 // We expect `fibonacci(0)` to return `0`, `fibonacci(1)` to return `1`,
 // `fibonacci(2)` to return `1`, and so on.
+
+static fibonacciNums = vec![0,1]
+
+
 pub fn fibonacci(n: u32) -> u32 {
-    // TODO: implement the `fibonacci` function
-    //
-    // Hint: use a `Vec` to memoize the results you have already calculated
-    // so that you don't have to recalculate them several times.
-    todo!()
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        2
+    }
 }
 
 #[cfg(test)]
