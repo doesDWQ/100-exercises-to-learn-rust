7d83c31fdc26caa8ae4038a61890319dff5d521a
dwq123 <2354513578@qq.com>
2026-01-10 09:58:16 +0800

    auto-commit

--- exercises/05_ticket_v2/07_unwrap/src/lib.rs
+++ exercises/05_ticket_v2/07_unwrap/src/lib.rs
@@ -2,7 +2,10 @@
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    switch Ticket::new(title, description, status) {
+        Ok(ticket) => ticket,
+        Err(err) => panic!("{}", err.unwrap()),
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
