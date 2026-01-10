67b82804528d5af34720cfbee8a4588804f1d9b5
dwq123 <2354513578@qq.com>
2026-01-10 10:14:50 +0800

    auto-commit

--- exercises/05_ticket_v2/08_error_enums/src/lib.rs
+++ exercises/05_ticket_v2/08_error_enums/src/lib.rs
@@ -1,7 +1,10 @@
 // TODO: Use two variants, one for a title error and one for a description error.
 //   Each variant should contain a string with the explanation of what went wrong exactly.
 //   You'll have to update the implementation of `Ticket::new` as well.
-enum TicketNewError {}
+enum TicketNewError {
+    TitleError(String),
+    DescriptionError(String),
+}
 
 // TODO: `easy_ticket` should panic when the title is invalid, using the error message
 //   stored inside the relevant variant of the `TicketNewError` enum.
