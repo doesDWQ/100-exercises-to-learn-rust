6db6d1229c4da014e5310b57589454bb47cc68c8
dwq123 <2354513578@qq.com>
2026-01-11 13:50:56 +0800

    auto-commit

--- exercises/06_ticket_management/14_index_mut/src/lib.rs
+++ exercises/06_ticket_management/14_index_mut/src/lib.rs
@@ -77,13 +77,13 @@ impl Index<&TicketId> for TicketStore {
 
 impl IndexMut<TicketId> for TicketStore {
     fn index_mut(&mut self, index: TicketId) -> &mut Self::Output {
-        self.tickets.iter_mut().find(|&t| t.id == index).unwrap()
+        self.tickets.iter_mut().find(|t| t.id == index).unwrap()
     }
 }
 
 impl IndexMut<&TicketId> for TicketStore {
     fn index_mut(&mut self, index: &TicketId) -> &mut Self::Output {
-        self.tickets.iter_mut().find(|&t| t.id == *index).unwrap()
+        self.tickets.iter_mut().find(|t| t.id == *index).unwrap()
     }
 }
 
