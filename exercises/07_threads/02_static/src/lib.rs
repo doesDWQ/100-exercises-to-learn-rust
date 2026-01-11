ade760bc03e808abeeafa86232d00656843f9861
dwq123 <2354513578@qq.com>
2026-01-11 16:24:36 +0800

    auto-commit

--- exercises/07_threads/02_static/src/lib.rs
+++ exercises/07_threads/02_static/src/lib.rs
@@ -1,10 +1,18 @@
 // TODO: Given a static slice of integers, split the slice into two halves and
 //  sum each half in a separate thread.
 //  Do not allocate any additional memory!
-use std::thread;
+use std::thread::{self, spawn};
 
 pub fn sum(slice: &'static [i32]) -> i32 {
-    todo!()
+    let (v1,v2) = slice.split_at(slice.len()/2);
+    let handle1 = spawn(move||{
+        v1.iter().sum::<i32>()
+    });
+    let handle2 = spawn(move||{
+        v2.iter().sum::<i32>()
+    });
+    
+    handle1.join().unwrap() + handle2.join().unwrap()
 }
 
 #[cfg(test)]
