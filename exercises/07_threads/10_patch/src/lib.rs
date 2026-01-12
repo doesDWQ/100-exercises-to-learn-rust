7777c6677872d34935109840e484b9b7885e6ade
dwq <2354513578@qq.com>
2026-01-12 21:32:52 +0800

    auto-commit

--- exercises/07_threads/10_patch/src/lib.rs
+++ exercises/07_threads/10_patch/src/lib.rs
@@ -89,7 +89,20 @@ pub fn server(receiver: Receiver<Command>) {
                 patch,
                 response_channel,
             }) => {
-                store.get_mut(patch.id).
+                let mut ret = false;
+                if let Some(ticket )= store.get_mut(patch.id){
+                    if let Some(title) = patch.title {
+                        ticket.title = title;
+                    }
+                    if let Some(description) = patch.description {
+                        ticket.description = description;
+                    }
+                    if let Some(status) = patch.status {
+                        ticket.status = status;
+                    }
+                    ret = true;
+                }
+                response_channel.try_send(ret).unwrap();
             }
             Err(_) => {
                 // There are no more senders, so we can safely break
