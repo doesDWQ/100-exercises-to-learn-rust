7fd4be6d2b0a20ed1fb8f0ae1873fd2248544f16
dwq <2354513578@qq.com>
2026-01-12 22:24:17 +0800

    auto-commit

--- exercises/07_threads/11_locks/src/store.rs
+++ exercises/07_threads/11_locks/src/store.rs
@@ -35,6 +35,11 @@ impl TicketStore {
     // The `get` method should return a handle to the ticket
     // which allows the caller to either read or modify the ticket.
     pub fn get(&self, id: TicketId) -> Option<Arc<Mutex<Ticket>>> {
-        self.tickets.get(id)
+        let x = self.tickets.get(&id);
+        if let Some(x1) = x {
+            Some(x1.clone())
+        } else {
+            None
+        }
     }
 }
